0800c555d0877a538c14656b92c762441f1e95d6
Angelits <aordaz@byupathway.com>
2025-01-16 17:03:30 -0600

    main website 1st assignment done

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = `© ${currentYear} Angela Ordaz Narváez, California`;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last time modified: ${lastModified}`;
+
+
