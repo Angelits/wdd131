9f14e6506c0a591465cd83d6503e095fd6a9d4ce
Angelits <aordaz@byupathway.com>
2025-03-17 09:20:35 -0600

    added the last modified time

--- scripts/temples.js
+++ scripts/temples.js
@@ -24,7 +24,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentYear = new Date().getFullYear();
     document.getElementById('currentyear').textContent = currentYear;
 
-    const currentDate = new Date();
+    
+    const lastModifiedDate = new Date(document.lastModified);
     const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    document.getElementById('lastModified').textContent = `Last modified: ${currentDate.toLocaleDateString('en-US', dateOptions)}`;
+    const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+
+
+    const lastModifiedText = `Last modified: ${lastModifiedDate.toLocaleDateString('en-US', dateOptions)} at ${lastModifiedDate.toLocaleTimeString('en-US', timeOptions)}`;
+    document.getElementById('lastModified').textContent = lastModifiedText;
 });
